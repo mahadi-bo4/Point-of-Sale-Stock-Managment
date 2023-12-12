223777fff480cc2bc7f04a4d6a78dd9e536a4e2d
mahadi-bo4 <mahadihasanbo4@gmail.com>
2023-12-12 22:44:50 +0600

    first commit

---
+++ src/pos/pro/PosPro.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pos.pro;
+
+public class PosPro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+    
+}
